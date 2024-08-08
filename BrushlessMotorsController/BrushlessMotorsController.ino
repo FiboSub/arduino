dc3f660db572602791ff01c76a77f2228ba5d9ef
thereisnoone893 <fatemehabibi893@gmail.com>
2024-08-08 14:52:59 +0330

    brushless motor test (ino file)

--- BrushlessMotorsController/BrushlessMotorsController.ino
+++ BrushlessMotorsController/BrushlessMotorsController.ino
@@ -1,17 +1,50 @@
 #include "BrushlessMotorsController.h"
 
+BrushlessMotorsController *motor;
+
 void setup()
-{ BrushlessMotorsController motor;
-    motor.changeSpeed(UP_RIGHT_MOTOR,1500);
-    delay(2000);
-    for(int i =1500; i <= 1900; i=i+50 ){
-            motor.changeSpeed(UP_RIGHT_MOTOR,i);
-            delay(2000);
-    }
-
-}
-void loop()
 {
-   
+    Serial.begin(9600);
 
+    motor = new BrushlessMotorsController();
+
+    motor->changeSpeed(UP_RIGHT_MOTOR, 1500);
+    delay(2000);
+
+
+    // for (int i = 1550; i <= 2000; i++)
+    // {
+    //     motor->changeSpeed(UP_RIGHT_MOTOR, i);
+    //     delay(1000);
+    // }
+
+    // for (int i = 1450; i <= 1150; i -= 50)
+    // {
+    //     motor->changeSpeed(UP_RIGHT_MOTOR, i);
+    //     delay(1000);
+    // }
 }
+
+void loop() {
+    if (Serial.available() > 0) {
+        long dc = Serial.parseInt();
+        Serial.read();
+        Serial.print("entered value == ");
+        Serial.print(dc);
+        motor->changeSpeed(UP_RIGHT_MOTOR, dc);
+        delay(500);
+    }
+}
+// {
+//     for (int i = 1100; i <= 1900; i = i + 50)
+//     {
+//         motor->changeSpeed(UP_RIGHT_MOTOR, i);
+//         delay(1000);
+//     }
+
+//     for (int i = 1900; i <= 1100; i -= 50)
+//     {
+//         motor->changeSpeed(UP_RIGHT_MOTOR, i);
+//         delay(1000);
+//     }
+// }
